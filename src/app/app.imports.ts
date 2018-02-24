850a12a0d97adcbf72dd20fa91739466648be124
gustavoviais <gv481792>
2018-02-24 15:59:49 -0300

    layout

--- src/app/app.imports.ts
+++ src/app/app.imports.ts
@@ -15,6 +15,7 @@ import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import {MatInputModule} from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';  
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes }  from '@angular/router'
 
 //DECLARATIONS
 import { FormatCurrencyPipe } from '../declarations/pipes/format-currency.pipe';
@@ -23,6 +24,12 @@ import { TasksListComponent } from '../pages/tasks/tasks-list/tasks.component';
 import { TasksNewComponent } from '../pages/tasks/tasks-new/tasks.component';
 import { CepComponent } from '../pages/cep/cep.component';
 import { MyTaskDirective } from '../declarations/directives/my-task.directive';
+
+//ROUTES
+const appRoutes:Routes = [
+    { path:'tasks', component:TasksListComponent },
+    { path:'tasks/new', component:TasksNewComponent },
+];
 
 export const PROVIDERS = [
     TaskService,
@@ -46,7 +53,8 @@ export const NATIVEMODULES = [
     MatInputModule,
     FormsModule,
     HttpModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
 ]
 
 export const DECLARATIONS = [
