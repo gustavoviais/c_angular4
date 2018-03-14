ccd25d0959dcd45f04bff55bda06e021c4545779
Gustavo Viais de Brito Silva <gustavo.silva@agfertipol.com.br>
2018-03-14 15:29:41 -0300

    2018-03-14

--- authentication/api/routes/api.php
+++ authentication/api/routes/api.php
@@ -17,6 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::group(['middleware' => 'cors'], function(){
     Route::group(['middleware' => 'jwt.auth'], function(){
         Route::get('products', 'Api\ProductsController@index');
